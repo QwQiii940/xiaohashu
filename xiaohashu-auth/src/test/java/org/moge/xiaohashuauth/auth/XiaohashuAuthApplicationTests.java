5e2f6b0e34c6b43f71bf0fa0ee25b8efd41f50c4
moge233 <mam2004@qq.com>
2026-09-16 17:23:28 +0800

    init: 小哈书（xiaohashu）微服务项目骨架

---
+++ xiaohashu-auth/src/test/java/org/moge/xiaohashuauth/auth/XiaohashuAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moge.xiaohashuauth.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaohashuAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
