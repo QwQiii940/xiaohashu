d67e7f37bc6846c2063705af015c54beca0c09da
moge233 <mam2004@qq.com>
2026-09-19 16:03:23 +0800

    init: 小哈书（xiaohashu）微服务项目骨架

--- xiaoha-framework/xiaoha-common/src/main/java/com/quanxiaoha/framework/common/exception/BaseExceptionInterface.java
+++
@@ -1,9 +0,0 @@
-package com.quanxiaoha.framework.common.exception;
-
-public interface BaseExceptionInterface {
-    // 获取异常码
-    String getErrorCode();
-
-    // 获取异常信息
-    String getErrorMessage();
-}
